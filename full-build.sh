218ce8be28f045895746af5d79d99ca9389e242c
Sean Seefried <sean.seefried@gmail.com>
2014-11-17 19:44:27 +1100

    Exit early if error.

--- full-build.sh
+++ full-build.sh
@@ -14,7 +14,7 @@ cp /home/androidbuilder/.ghc/android-14/arm-linux-androideabi-4.8/cabal/lib/arm-
   $THIS_DIR/jni/epidemic-libs/armeabi/libHSEpidemic-0.0.1.a
 
 ndk-build clean && ndk-build -j9 && ant debug
-
+[ $? -eq 0 ] || exit 1
 
 
 scp $THIS_DIR/bin/$APK 'mac:~/tmp'
