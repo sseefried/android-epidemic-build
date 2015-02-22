f4fbde4aceed0e343e5a8d124e95ce903b2f6dca
Sean Seefried <sean.seefried@gmail.com>
2015-02-22 21:20:29 +1100

    Change build release and add signing information.

--- full-build.sh
+++ full-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source $HOME/.ndkrc
 
-APK=com.declarative.games.epidemic-debug.apk
+APK=com.declarative.games.epidemic-release.apk
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ~/ghc-build/epidemic-game
@@ -13,7 +13,7 @@ cd $THIS_DIR
 cp /home/androidbuilder/.ghc/android-14/arm-linux-androideabi-4.8/cabal/lib/arm-linux-android-ghc-7.8.2/Epidemic-0.0.1/libHSEpidemic-0.0.1.a \
   $THIS_DIR/jni/epidemic-libs/armeabi/libHSEpidemic-0.0.1.a
 
-ndk-build clean && ndk-build -j9 && ant debug
+ndk-build clean && ndk-build -j9 && ant release
 [ $? -eq 0 ] || exit 1
 
 scp $THIS_DIR/bin/$APK 'mac:~/tmp'
